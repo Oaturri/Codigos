0e47136e2c3779c4cc55655f04bdb0869b80f3a5
Oaturri <117463844+Oaturri@users.noreply.github.com>
2023-09-27 09:39:37 -0500

    Update ProyectoAyP (interfaz grafica).py

--- ProyectoAyP (interfaz grafica).py
+++ ProyectoAyP (interfaz grafica).py
@@ -1,23 +1,26 @@
 import tkinter as tk
 from tkinter import simpledialog, messagebox
 
-objetos = []
+objetos = []  # Lista para almacenar objetos
 
+# Función para registrar usuarios
 def registrar_usuario():
-    nombre = simpledialog.askstring("Registro", "Introduce tu nombre:")
-    edad = simpledialog.askinteger("Registro", "Introduce tu edad:")
-    while edad < 18:
+    nombre = simpledialog.askstring("Registro", "Introduce tu nombre:")  # Solicitar nombre
+    edad = simpledialog.askinteger("Registro", "Introduce tu edad:")  # Solicitar edad
+    while edad < 18:  # Verificar que el usuario sea mayor de 18
         messagebox.showerror("Error", "Debes tener al menos 18 años para registrarte.")
         edad = simpledialog.askinteger("Registro", "Introduce tu edad:")
     carnet_estudiantil = simpledialog.askstring("Registro", "Introduce tu carnet estudiantil:")
 
+# Función para publicar objetos
 def publicar_objeto():
-    nombre = simpledialog.askstring("Publicar Objeto", "Nombre del objeto:")
-    descripcion = simpledialog.askstring("Publicar Objeto", "Descripción:")
-    precio = simpledialog.askfloat("Publicar Objeto", "Precio:")
-    cantidad = simpledialog.askinteger("Publicar Objeto", "Cantidad:")
+    nombre = simpledialog.askstring("Publicar Objeto", "Nombre del objeto:")  # Solicitar nombre del objeto
+    descripcion = simpledialog.askstring("Publicar Objeto", "Descripción:")  # Solicitar descripción
+    precio = simpledialog.askfloat("Publicar Objeto", "Precio:")  # Solicitar precio
+    cantidad = simpledialog.askinteger("Publicar Objeto", "Cantidad:")  # Solicitar cantidad
     objetos.append({"nombre": nombre, "descripcion": descripcion, "precio": precio, "cantidad": cantidad})
 
+# Función para revisar objetos y realizar compras
 def revisar_objetos():
     lista_objetos = "\n".join([f"{i+1}. {obj['nombre']} - {obj['descripcion']} - Precio: {obj['precio']} - Cantidad: {obj['cantidad']}" for i, obj in enumerate(objetos)])
     opcion = simpledialog.askinteger("Revisar Objetos", f"{lista_objetos}\n\nSelecciona el número del objeto del cual deseas comprar o 0 para regresar:")
@@ -30,7 +33,7 @@ def revisar_objetos():
             objetos[opcion-1]['cantidad'] -= cantidad_a_quitar
 
             if objetos[opcion-1]['cantidad'] <= 0:
-                objetos.pop(opcion - 1)
+                objetos.pop(opcion - 1)  # Eliminar objeto si se agota el stock
                 messagebox.showinfo("Información", "Objeto eliminado por falta de stock.")
             else:
                 messagebox.showinfo("Información", f"Quedan {objetos[opcion-1]['cantidad']} unidades.")
@@ -41,6 +44,7 @@ def revisar_objetos():
     else:
         messagebox.showerror("Error", "Opción no válida.")
 
+# Función principal para mostrar el menú
 def menu():
     registrar_usuario()
     while True:
@@ -57,4 +61,4 @@ def menu():
 
 root = tk.Tk()
 root.withdraw()  # Oculta la ventana principal de tkinter
-menu()
+menu()  # Iniciar el programa llamando a la función menu()
